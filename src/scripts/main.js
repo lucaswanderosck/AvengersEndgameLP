59dc934ad8c2009494a90ef47ed48cba1e0a4b37
lucas wanderosck <lucaskleinwanderosck@gmail.com>
2023-06-19 14:41:50 -0300

    criando o hero

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,12 @@
 document.addEventListener("DOMContentLoaded", function () {
   AOS.init();
+
+// Efeito para o scroll no header usando jquery
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 0) {
+      $(".header").addClass("header--scrolled");
+    } else {
+      $(".header").removeClass("header--scrolled");
+    }
+  });
 });
